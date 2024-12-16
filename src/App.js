2ef88db0da8501ada177143be4342e4df58f16df
Jacky HE <ihkk.nb@gmail.com>
2024-12-16 18:22:21 +0800

    feat: update language detection logic for improved handling of Chinese text

--- src/App.js
+++ src/App.js
@@ -86,9 +86,10 @@ function App() {
     let lang;
     if (text.indexOf("出発地") !== -1) {
       lang = "JP";
-    } else if (text.indexOf("已保存") !== -1) {
+    } else if (text.indexOf("图层") !== -1) {
       lang = "CN";
     }
+
 
     let departureMatch;
     if (lang === "JP") {
@@ -203,7 +204,6 @@ function App() {
         } else if (lang === "CN") {
           mode = mode.replace(/(.*线)/g, "$1 ").replace(/(.*Line)/g, "$1 ").replace(/(.*線)/g, "$1 ").replace(/^(地铁)/, "").replace(/^(路面电车)/, "").replace(/(.*号)/g, "$1 ").replace(/^(火车)/, "").replace(/^(新干线)/, "").replace(/步行步行/g, "").replace(/^(公交)/, "公交：").replace(/^(轮渡)/, "轮渡：");
           mode = mode.replace(/(通勤快急|通勤急行|区間特急|快速急行|空港急行|直通特急|通勤特急|新快速|特快速|准特急|特急线|各站停车|各停|快特|普通|急行|快速|特急)/g, "$1 ");
-
         }
 
         // check platform for the i+5 line
